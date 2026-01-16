809934a8f1590e388ea64ca9458ce31283b84a42
Mike Wyant Jr. <mike@wyantswalk.net>
2026-01-16 12:43:15 -0500

    chore: add custom element typing for diffs

--- packages/app/src/custom-elements.d.ts
+++ packages/app/src/custom-elements.d.ts
@@ -1,1 +1,9 @@
-../../ui/src/custom-elements.d.ts
+import { DIFFS_TAG_NAME } from "@pierre/diffs"
+
+declare module "solid-js" {
+  namespace JSX {
+    interface IntrinsicElements {
+      [DIFFS_TAG_NAME]: HTMLAttributes<HTMLElement>
+    }
+  }
+}
